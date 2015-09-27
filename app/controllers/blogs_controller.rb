c31d618f4b3496e6fd18491d4eb90cb7bb9b0798
Alice Chiu <alicejchiu@gmail.com>
2015-09-27 16:06:09 -0700

    updates for final project

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,20 @@
 class BlogsController < ApplicationController
 
+  def index
+    @blogs = Blog.all
+  end
+
   def new
+    @blog = Blog.new
   end
 
   def create
-    render plain: params[:blogs].inspect
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render "new"
+    end
   end
 
   private
